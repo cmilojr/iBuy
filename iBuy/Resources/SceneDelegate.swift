dd44987f4c72ee603ef990c8cc0376988fed07a6
Juan Camilo Jiménez Rojas <jcjimenezr@eafit.edu.co>
2022-01-06 23:43:39 -0500

    pulido

--- iBuy/Resources/SceneDelegate.swift
+++ iBuy/Resources/SceneDelegate.swift
@@ -8,63 +8,86 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
         guard let winScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: winScene)
-        if let _ = try! Storage.shared.getLocalCountry()  {
-            guard let rootVC = UIStoryboard
-                    .init(name: Constants.SBIdentifier.dashboard, bundle: nil)
-                    .instantiateViewController(identifier: Constants.SBIdentifier.dashboard) as? DashboardVC else {return}
-
-            let rootNC = UINavigationController(rootViewController: rootVC)
-            self.window?.rootViewController = rootNC
-            self.window?.makeKeyAndVisible()
-        } else {
-            guard let rootVC = UIStoryboard
-                    .init(name: Constants.SBIdentifier.selectCountry, bundle: nil)
-                    .instantiateViewController(identifier: Constants.SBIdentifier.selectCountry) as? SelectCountryVC else {return}
-
-            let rootNC = UINavigationController(rootViewController: rootVC)
-            self.window?.rootViewController = rootNC
-            self.window?.makeKeyAndVisible()
-        }
+        
+        let chooseVc = ChooseVC()
+        let vc = chooseVc.getInitialViewController()
+        let nc = vc.getNavController(for: self.window!)
+        let rootNc = UINavigationController(rootViewController: nc)
+        self.window?.rootViewController = rootNc
+        self.window?.makeKeyAndVisible()
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
+    
+    
 }
 
+protocol InitialViewControllerProtocol {
+    func getNavController(for: UIWindow) -> UINavigationController
+}
+
+struct SelectDashboard: InitialViewControllerProtocol {
+    func getNavController(for: UIWindow) -> UINavigationController {
+        let rootVC = UIStoryboard
+            .init(name: Constants.SBIdentifier.dashboard, bundle: nil)
+            .instantiateViewController(identifier: Constants.SBIdentifier.dashboard) as? DashboardVC
+        return UINavigationController(rootViewController: rootVC!)
+    }
+}
+
+struct SelectCountry: InitialViewControllerProtocol {
+    func getNavController(for: UIWindow) -> UINavigationController {
+        let rootVC = UIStoryboard
+            .init(name: Constants.SBIdentifier.selectCountry, bundle: nil)
+            .instantiateViewController(identifier: Constants.SBIdentifier.selectCountry) as? SelectCountryVC
+        return UINavigationController(rootViewController: rootVC!)
+    }
+}
+
+struct ChooseVC {
+    func getInitialViewController() -> InitialViewControllerProtocol {
+        do {
+            let _ = try Storage.shared.getLocalCountry()
+            return SelectDashboard()
+        } catch {
+            return SelectCountry()
+        }
+    }
+}
